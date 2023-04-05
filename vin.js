7dd146bf14821df5f98721ad9fe0c5a0b60fbe96
vincenttommi <vincenttommi@gmail.com>
2023-04-05 09:25:02 +0300

    addind footer

---
+++ vin.js
@@ -0,0 +1,17 @@
+
+const riding = document.getElementsByClassName('container-deal');
+fetch("http://localhost:3000/bikes")
+// fetching out data through db.json network file
+
+.then(response => response.json())
+.then(data => {
+
+
+//loopingdata from our array db.json file to
+//html elements
+
+
+
+
+})
+
