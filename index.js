05a1257a52602e81f900a636b7cb71225e9c6fa8
vincenttommi <vincenttommi@gmail.com>
2023-04-05 19:32:22 +0300

    validating my form

--- index.js
+++ index.js
@@ -1,74 +1,59 @@
-// Select the ul#films element
-const bikeList = document.querySelector('#bikes');
+let slideIndex = 1;
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("carousel-item");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+setInterval(() => {
+  plusSlides(1);
+}, 3000);
 
-// Fetch the movie data
-fetch(" http://localhost:4580/bikes")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    // Loop through each movie in the data and add it to the filmList
-    data.forEach(function (bikes) {
-      const listItem = document.createElement('li');
-      listItem.classList.add('bike', 'item');
-      listItem.textContent = bike.Bike_type;
-      bikeList.appendChild(listItem);
-    });
 
-    // Select the  div details
-    const bikes = document.querySelector('.details');
+//validating form using a function
+function  validate(){
+ console.log("name");
 
-    // Loop through each movie in the data and create a new card for it
-    data.forEach(function (bike) {
-      const card = document.createElement('div');
-      card.classList.add('card');
-      card.innerHTML = `
-        <div class="container">
-          <img src="${bike.image}" alt="Product Image" style="width:50%; height:50%">
-          <h1><b>${bike.Bike_type}</b></h1>
-          <p>Time: ${bike.Time}</p>
-          <p>Hiring_Price: ${bike.Hiring_Price}</p>
+  const form = document.getElementById("#myform");
 
-          <p>Available Bikes: <span class="available-bikes">${bike.capacity - Hired_Bikes}</span></p>
-          <button class="Hired_Bikes">Hired_Bikes</button>
-        </div>
-      `;
-      bikes.appendChild(card);
+  //capturing id fo the form
 
-      // Add a click event listener to the "Hire Ticket" button
-      const HireBike = card.querySelector('.Hired_Bikes');
-      HireBike.addEventListener('click', function(event) {
-        event.preventDefault(); // Prevent the page from reloading
-        // Check if there are any available bikes
-        const availableBikes = card.querySelector('.available-bikes');
-        const numAvailablebikes = parseInt(availablebikes.textContent);
-        if (numAvailablebikes > 0) {
-          // Update the number of available bikes and display it on the frontend
-          availableBikes.textContent = numAvailablebikes - 1;
 
-          // Update the bikes data in the backend
-          const newHiredBikes = bike.tickets_sold + 1;
-          fetch(`http://localhost:4580/bikes${bike.id}`, {
-            method: 'PATCH',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ Hired_Bikes: newHiredBikes })
-          })
-            .then(function(response) {
-              if (!response.ok) {
-                throw new Error('Network response was not ok');
-              }
-            })
-            .catch(function(error) {
-              console.error('There was a problem updating the bike data:', error);
-            });
-        } else {
-          alert('Sorry, this showing is hired bikes');
-        }
-      });
-    });
-  })
-  .catch(function(error) {
-    console.log('There was an error fetching the bike data:', error);
-  });
+  const firstname  = document.getElementById("firstname").value;
+  const lastname = document.getElementById("lastname").value;
+  const username = document.getElementById("username").value;
+  const  city = document.getElementById("city").value;
+  const estate = document.getElementById("estate").value;
+  const zip = document.getElementById("zip").value;
+  const invalidCheck2 = document.getElementById("invalidCheck2").value;
+
+
+
+
+  if(firstname==""|| lastname =="" || username == "" || city =="" ||estate ==""||zip == ""|| invalidCheck2 ==""){
+
+
+    Swal.fire('oops','all fields are required','error');
+
+  }else{
+
+
+}
+}
